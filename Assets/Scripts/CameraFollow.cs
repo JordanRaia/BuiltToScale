3441a76e95b375919f4a3476313d19e94087b2eb
Jordan Raia <jordanraiapi@gmail.com>
2024-08-17 10:50:28 -0500

    camera zoom matches size of player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public float cameraDistance = 5f; // Default distance of the camera from the player
+    public float cameraHeight = 1f;   // Default height of the camera above the player
+    public Camera cam;
+
+    private float initialOrthographicSize;
+
+    void Start()
+    {
+        // Store the initial orthographic size of the camera
+        initialOrthographicSize = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the current orthographic size based on the player's scale
+        float playerScale = player.localScale.x;
+        cam.orthographicSize = initialOrthographicSize * playerScale;
+
+        // Adjust the camera's position to follow the player
+        Vector3 offset = new Vector3(0, cameraHeight, -cameraDistance);
+        transform.position = player.position + offset;
+    }
+}
